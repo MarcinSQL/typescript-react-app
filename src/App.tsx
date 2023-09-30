8320071743813b05fd1519a610f232f9156f61ba
Marcin Wasilewski <marcin.wasilewski512@gmail.com>
2023-09-30 12:09:22 +0200

    Deleted some init files and created bare Person component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {FC} from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App: FC = () => {
+  //TS deff
+  // const name: string = "Pedro";
+  // const age: number = 32;
+  // const isMarried: boolean = false;
+
+  // const getName = (name: string): number => {
+  //   if(name === "Pedro"){
+  //     return 20;
+  //   } else {
+  //     return 0;
+  //   }
+  // }
+
+
+
+  return <div className="App"></div>;
 }
 
 export default App;
---
+++ src/components/Person.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+export interface IPersonProps {}
+
+export const Persone = (props: IPersonProps) => {
+  return <div></div>;
+};
